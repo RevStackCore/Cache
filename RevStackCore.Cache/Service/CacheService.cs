27e9d125a6f14d80414adfda969356a4221d4f2c
Scott <sfrancis@misinteractive.com>
2017-02-18 02:48:03 -0500

    virtualized service methods

--- RevStackCore.Cache/Service/CacheService.cs
+++ RevStackCore.Cache/Service/CacheService.cs
@@ -13,52 +13,52 @@ namespace RevStackCore.Cache
 			_repository = repository;
 		}
 
-		public void Delete(string key)
+		public virtual void Delete(string key)
 		{
 			_repository.Delete(key);
 		}
 
-		public Task DeleteAsync(string key)
+		public virtual Task DeleteAsync(string key)
 		{
 			return Task.Run(() => Delete(key));
 		}
 
-		public TEntity Get(string key)
+		public virtual TEntity Get(string key)
 		{
 			return _repository.Get(key);
 		}
 
-		public Task<TEntity> GetAsync(string key)
+		public virtual Task<TEntity> GetAsync(string key)
 		{
 			return Task.FromResult(Get(key));
 		}
 
-		public IEnumerable<TEntity> GetCollection(string key)
+		public virtual IEnumerable<TEntity> GetCollection(string key)
 		{
 			return _repository.GetCollection(key);
 		}
 
-		public Task<IEnumerable<TEntity>> GetCollectionAsync(string key)
+		public virtual Task<IEnumerable<TEntity>> GetCollectionAsync(string key)
 		{
 			return Task.FromResult(GetCollection(key));
 		}
 
-		public void Set(TEntity entity, string key)
+		public virtual void Set(TEntity entity, string key)
 		{
 			_repository.Set(entity, key);
 		}
 
-		public void Set(TEntity entity, string key, TimeSpan timeSpan)
+		public virtual void Set(TEntity entity, string key, TimeSpan timeSpan)
 		{
 			_repository.Set(entity, key, timeSpan);
 		}
 
-		public Task SetAsync(TEntity entity, string key)
+		public virtual Task SetAsync(TEntity entity, string key)
 		{
 			return Task.Run(() => Set(entity, key));
 		}
 
-		public Task SetAsync(TEntity entity, string key, TimeSpan timeSpan)
+		public virtual Task SetAsync(TEntity entity, string key, TimeSpan timeSpan)
 		{
 			return Task.Run(() => Set(entity, key,timeSpan));
 		}
